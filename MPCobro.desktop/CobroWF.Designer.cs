8ee39ab9ae38c670095077e740a56b267d77a2a2
HelenKBC <helen.contreras@esfe.agape.edu.sv>
2023-11-08 11:07:57 -0600

    Este si es

--- MPCobro.desktop/CobroWF.Designer.cs
+++ MPCobro.desktop/CobroWF.Designer.cs
@@ -67,14 +67,14 @@
             this.cbxEmpleado.Location = new System.Drawing.Point(31, 55);
             this.cbxEmpleado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbxEmpleado.Name = "cbxEmpleado";
-            this.cbxEmpleado.Size = new System.Drawing.Size(307, 33);
+            this.cbxEmpleado.Size = new System.Drawing.Size(438, 33);
             this.cbxEmpleado.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(464, 22);
+            this.label2.Location = new System.Drawing.Point(29, 111);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 25);
             this.label2.TabIndex = 5;
@@ -85,16 +85,16 @@
             this.cbxLocal.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbxLocal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxLocal.FormattingEnabled = true;
-            this.cbxLocal.Location = new System.Drawing.Point(468, 55);
+            this.cbxLocal.Location = new System.Drawing.Point(33, 144);
             this.cbxLocal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbxLocal.Name = "cbxLocal";
-            this.cbxLocal.Size = new System.Drawing.Size(307, 33);
+            this.cbxLocal.Size = new System.Drawing.Size(436, 33);
             this.cbxLocal.TabIndex = 4;
             // 
             // dpFechaCobro
             // 
             this.dpFechaCobro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dpFechaCobro.Location = new System.Drawing.Point(31, 155);
+            this.dpFechaCobro.Location = new System.Drawing.Point(32, 240);
             this.dpFechaCobro.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dpFechaCobro.Name = "dpFechaCobro";
             this.dpFechaCobro.Size = new System.Drawing.Size(437, 30);
@@ -104,7 +104,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(28, 123);
+            this.label3.Location = new System.Drawing.Point(29, 208);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(279, 29);
             this.label3.TabIndex = 10;
@@ -135,12 +135,12 @@
             dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dgvCobro.DefaultCellStyle = dataGridViewCellStyle3;
-            this.dgvCobro.Location = new System.Drawing.Point(31, 207);
+            this.dgvCobro.Location = new System.Drawing.Point(521, 22);
             this.dgvCobro.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvCobro.Name = "dgvCobro";
             this.dgvCobro.RowHeadersWidth = 51;
             this.dgvCobro.RowTemplate.Height = 24;
-            this.dgvCobro.Size = new System.Drawing.Size(745, 290);
+            this.dgvCobro.Size = new System.Drawing.Size(803, 360);
             this.dgvCobro.TabIndex = 19;
             // 
             // Editar
@@ -187,10 +187,10 @@
             // 
             this.btnModificar.BackColor = System.Drawing.SystemColors.Control;
             this.btnModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnModificar.Location = new System.Drawing.Point(503, 123);
+            this.btnModificar.Location = new System.Drawing.Point(31, 325);
             this.btnModificar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(273, 64);
+            this.btnModificar.Size = new System.Drawing.Size(438, 41);
             this.btnModificar.TabIndex = 22;
             this.btnModificar.Text = "Modificar";
             this.btnModificar.UseVisualStyleBackColor = false;
@@ -200,10 +200,10 @@
             // 
             this.btnGuardar.BackColor = System.Drawing.SystemColors.Control;
             this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGuardar.Location = new System.Drawing.Point(503, 142);
+            this.btnGuardar.Location = new System.Drawing.Point(31, 325);
             this.btnGuardar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(273, 46);
+            this.btnGuardar.Size = new System.Drawing.Size(438, 42);
             this.btnGuardar.TabIndex = 21;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = false;
@@ -212,7 +212,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(877, 543);
+            this.ClientSize = new System.Drawing.Size(1357, 413);
             this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.dgvCobro);
